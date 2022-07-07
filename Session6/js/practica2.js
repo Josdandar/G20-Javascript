c05e868c1aa7c74d3ba12e3da6bac4b05809b21a
Jose De Anda <josdandar@gmail.com>
2022-07-06 21:07:12 -0700

    Added files to Session6

---
+++ Session6/js/practica2.js
@@ -0,0 +1,13 @@
+let string1 = "Hola"
+let string2 = "a"
+let string3 = "todos."
+
+
+
+function greetings(input1, input2, input3){
+    let frase = ""
+    frase = `${input1} ${input2} ${input3}`
+    return frase
+}
+
+let frasefull = greetings(string1, string2, string3)
