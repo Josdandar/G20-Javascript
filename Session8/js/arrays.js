65575007482180c942f985571736e66530684bf4
Jose De Anda <josdandar@gmail.com>
2022-07-11 14:42:24 -0700

    Added files

---
+++ Session8/js/arrays.js
@@ -0,0 +1,17 @@
+let string = "hey"
+let times = 3
+let result = ""
+
+
+
+
+
+const repeatString = function(stringg, timess) {
+    for (let index = 0; index < times; index++) {
+        result += string
+        
+    }console.log(result)
+};
+repeatString(string, times)
+// Do not edit below this line
+module.exports = repeatString;
